14cb24c1529cdd2e86a19f4438a0a972f69099e2
Danglinh206 <153258521+Danglinh206@users.noreply.github.com>
2024-09-07 16:51:58 +0700

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const botToken = '6786693118:AAGEZ3MDC7HgHOrSB2x1eW8E4DpPVooj0x8';
 const bot = new TelegramBot(botToken, {polling: true});
 
-const adminIds = ['5930542560']; 
+const adminIds = ['-1001928617056']; 
 
 fs.readFile('appstate.json', 'utf8', (err, data) => {
   if (err) {
